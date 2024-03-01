a53b2192273830abb57ce76ec9c124b32a9bbd08
jprietop <jopriepei@alu.edu.gva.es>
2024-03-01 16:16:49 +0100

    1.0

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,8 @@ import 'package:agro_sync/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:agro_sync/components/my_button.dart';
 import 'package:agro_sync/components/my_textfield.dart';
-import 'package:colorful_background/colorful_background.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:metaballs/metaballs.dart';
 
 import '../petittions_http.dart';
 
@@ -43,118 +43,78 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: ColorfulBackground(
-        duration: const Duration(milliseconds: 1000),
-        backgroundColors: const [
-          Color(0xFF121212),
-          Color(0xFF121212),
-        ],
-        decoratorsList: [
-          Positioned(
-            top: MediaQuery.of(context).size.height / 2,
-            right: MediaQuery.of(context).size.width / 2.5,
-            child: Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.3),
-                shape: BoxShape.circle,
-              ),
+      body: Stack(
+        children: [
+          Metaballs(
+            color: const Color.fromARGB(255, 66, 133, 244),
+            effect: MetaballsEffect.follow(
+              growthFactor: 1,
+              smoothing: 1,
+              radius: 0.5,
             ),
-          ) ,
-          Positioned(
-            top: 40,
-            right: 20,
-            child: Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                color: Colors.yellow.withOpacity(0.3),
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 200,
-            right: 90,
-            child: Container(
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                color: Colors.red.withOpacity(0.3),
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height / 2,
-            right: 90,
-            child: Container(
-              height: 80,
-              width: 80,
-              decoration: BoxDecoration(
-                color: Colors.purple.withOpacity(0.3),
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height / 1.45,
-            right: 90,
-            child: Container(
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.3),
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-        ],
-      child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 50),
-                Text(
-                  'AgroSync',
-                  style: GoogleFonts.majorMonoDisplay(
-                    color: Colors.white,
-                    fontSize: 50,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 150),
-                MyTextField(
-                  controller: usernameController,
-                  hintText: 'Usuario',
-                  obscureText: false,
-                  suffixIconData: Icons.person,
-                ),
-                const SizedBox(height: 15),
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Contraseña',
-                  obscureText: true,
-                  suffixIconData: Icons.lock,
-                ),
-                const SizedBox(height: 100),
-                MyButton(
-                    onTap: () => signInUser(context),
-                ),
-                const SizedBox(height: 15),
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 255, 0),
+                Color.fromARGB(255, 128, 0, 128),
               ],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+            metaballs: 40,
+            animationDuration: const Duration(milliseconds: 200),
+            speedMultiplier: 1,
+            bounceStiffness: 3,
+            minBallRadius: 15,
+            maxBallRadius: 40,
+            glowRadius: 0.7,
+            glowIntensity: 0.6,
+            child: const SizedBox.expand(),
+          ),
+
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 50),
+                  Text(
+                    'AgroSync',
+                    style: GoogleFonts.majorMonoDisplay(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 150),
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Usuario',
+                    obscureText: false,
+                    suffixIconData: Icons.person,
+                  ),
+                  const SizedBox(height: 15),
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Contraseña',
+                    obscureText: true,
+                    suffixIconData: Icons.lock,
+                  ),
+                  const SizedBox(height: 100),
+                  MyButton(
+                    onTap: () => signInUser(context),
+                  ),
+                  const SizedBox(height: 15),
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
